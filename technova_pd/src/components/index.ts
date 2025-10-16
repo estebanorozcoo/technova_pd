59090e0fcc01d1bd9662c86f88d0ae368c8567f7
estebanorozcoo <138067495+estebanorozcoo@users.noreply.github.com>
2025-10-16 06:33:24 -0500

    Delete technova_pd directory

--- technova_pd/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-// Exportación centralizada de componentes
-export { default as Button } from './ui/Button';
-export { default as Badge } from './ui/Badge';
-export { default as Card } from './ui/Card';
--- technova_pd/src/services/index.ts
+++
@@ -1,4 +0,0 @@
-// services/index.ts
-export { default as api } from './api';
-export { productService } from './productService';
-export { authService } from './authService';
